cf915173455904ec4e6e5fdad82385be91477b52
Your Name <you@example.com>
2021-01-22 15:35:25 +0600

    init

---
+++ main/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, HttpResponse
+
+def homepage(request):
+    return render(request, "index.html")
+
+
+def test(request):
+    return render(request, "test.html")
+
+def second(request):
+    return HttpResponse("test 2 page")
+
+def third(request):
+    return HttpResponse("test 3 page")
