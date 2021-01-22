b7de0d23ee8ff6156426c2886e5bfc37bf8f91dd
Your Name <you@example.com>
2021-01-22 18:05:25 +0600

    set static and media urls and roots.

--- main/views.py
+++ main/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, HttpResponse
 
 def homepage(request):
-    return render(request, "index.html")
+    return render(request, "indexc.html")
 
 
 def test(request):
