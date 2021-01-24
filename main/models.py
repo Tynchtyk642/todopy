6ff98dff87a9b13be0288b02fd7d472d658c6010
Your Name <you@example.com>
2021-01-24 14:06:32 +0600

    add books.html according to 32 lession

--- main/models.py
+++ main/models.py
@@ -5,3 +5,17 @@ class ToDo(models.Model):
     created_at = models.DateField(auto_now_add=True)
     is_closed = models.BooleanField(default=False)
     is_favorite = models.BooleanField(default=False)
+
+class Book(models.Model):
+    title = models.CharField(max_length=50)
+    subtitle = models.CharField(max_length=50)
+    description = models.TextField(max_length=50)
+    price = models.IntegerField()
+    genre = models.CharField(max_length=50)
+    author = models.CharField(max_length=100)
+    year = models.DateField()
+    
+
+
+
+
--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, HttpResponse
-from .models import ToDo
+from .models import ToDo, Book
 
 def homepage(request):
     return render(request, "indexc.html")
@@ -14,3 +14,6 @@ def second(request):
 
 def third(request):
     return HttpResponse("test 3 page")
+
+def books(request):
+    return render(request, "books.html")
